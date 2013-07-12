03c23135cf09d57c5555fa55f77e659314545a5f
kasiratnamkaranam <kasiratnam1987@gmail.com>
2013-07-12 14:43:53 +0530

    simple Calucations

--- SampleCalculator/ViewController.h
+++ SampleCalculator/ViewController.h
@@ -7,7 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *enterValue1;
+@property (weak, nonatomic) IBOutlet UITextField *enterValue2;
+@property (weak, nonatomic) IBOutlet UILabel *resultDisplay;
+//@property (nonatomic, retain) NSMutableArray *resultArray;
+- (IBAction)addition:(id)sender;
+- (IBAction)subtraction:(id)sender;
+- (IBAction)multiplication:(id)sender;
+- (IBAction)division:(id)sender;
+- (IBAction)faceBook:(id)sender;
 
 @end
--- SampleCalculator/ViewController.m
+++ SampleCalculator/ViewController.m
@@ -13,11 +13,14 @@
 @end
 
 @implementation ViewController
+@synthesize enterValue1,enterValue2,resultDisplay;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    //resultArray = [[NSMutableArray alloc]init];
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +29,119 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)addition:(id)sender {
+    
+    // intialize and enter the input variables a and b
+    
+    int a = [self.enterValue1.text intValue];
+    int b = [self.enterValue2.text intValue];
+    
+    // Perform addition and store the result in variable c
+    
+    int c = a+b;
+    
+    //Print the result in console
+    
+    NSLog(@"Addition output = %d",c);
+    
+    //Displays the result in display label
+    
+    self.resultDisplay.text = [NSString stringWithFormat:@"Result = %d",c];
+}
+
+- (IBAction)subtraction:(id)sender {
+    
+    // intialize and enter the input variables a and b
+    
+    int a = [self.enterValue1.text intValue];
+    int b = [self.enterValue2.text intValue];
+    
+    // Perform subtraction and store the subtraction result in variable c
+    
+    int c = a-b;
+    
+    // Print the result in console
+    
+    NSLog(@"Subtraction output = %d",c);
+    
+    // Displays the result in label
+    
+    self.resultDisplay.text = [NSString stringWithFormat:@"Result = %d",c];
+}
+
+- (IBAction)multiplication:(id)sender {
+    
+    // intialize and enter the input variables a and b
+    
+    int a = [self.enterValue1.text intValue];
+    int b = [self.enterValue2.text intValue];
+    
+    // Perform multiplication and store the result in variable c
+    
+    int c = a*b;
+    
+    // Print the result in console
+    
+    NSLog(@"Multiplication output = %d",c);
+    
+    // Display The result in label
+    
+    self.resultDisplay.text = [NSString stringWithFormat:@"Result = %d",c];
+    
+    
+}
+
+- (IBAction)division:(id)sender {
+    
+    // intialize and enter the a and b values
+    
+    float a = [self.enterValue1.text intValue];
+    float b = [self.enterValue2.text intValue];
+    
+    // Perform division operation and store the result in c
+    
+    float c = a/b;
+    
+    //Print the result in console
+    
+    NSLog(@"Division Output = %f",c);
+    
+    // Display the division Output in label
+    
+    self.resultDisplay.text =[NSString stringWithFormat:@"Result = %f",c];
+}
+
+- (IBAction)faceBook:(id)sender {
+    
+    if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeFacebook]) {
+        SLComposeViewController *controller = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeFacebook];
+        SLComposeViewControllerCompletionHandler myFacebook = ^(SLComposeViewControllerResult result){
+            
+            if (result == SLComposeViewControllerResultCancelled) {
+                NSLog(@"Cancelled");
+            }else{
+                NSLog(@"Done");
+            }
+            
+            [controller dismissViewControllerAnimated:YES completion:nil];
+        };
+        
+        controller.completionHandler = myFacebook;
+        
+        // Adding the text to the faceBook Post value from iOS
+        
+        [controller setInitialText:@"Result"];
+        [controller setInitialText:self.resultDisplay.text];
+        
+        [self presentViewController:controller animated:YES completion:nil];
+    }
+    
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+    //Resign the keypad in view
+    
+    [self.view endEditing:YES];
+}
 @end
